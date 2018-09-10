cc390a0b8de77b2770abc2b940ac8546fd1ea4e0
abhijithneilabraham <35420019+abhijithneilabraham@users.noreply.github.com>
2018-09-10 14:12:35 +0530

    Update oddoreven_env.py

--- gym-oddoreven/envs/oddoreven_env.py
+++ gym-oddoreven/envs/oddoreven_env.py
@@ -5,9 +5,18 @@ from gym.utils import seeding
 class OddorevenEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
-  def __init__(self):
+  def __init__(self,small=2,large=10):
+    self.input[]
+    self.state=0
+    self.seed=0#I am specifying the reard small as 2 and large as 10
+
+  def seed(self, seed=None):
+    self.np_random, seed = seeding.np_random(seed)
+    return [seed]    
+   
   
   def _step(self, action):
+    if self.episodes
     
   def _reset(self):
     
