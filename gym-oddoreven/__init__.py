f34ef2d785245e5cac709723a52546f9c31e52ec
abhijithneilabraham <35420019+abhijithneilabraham@users.noreply.github.com>
2018-09-10 11:34:02 +0530

    Update __init__.py

--- gym-oddoreven/__init__.py
+++ gym-oddoreven/__init__.py
@@ -1,1 +1,11 @@
 
+from gym.envs.registration import register
+
+register(
+    id='oddoreven-v0',
+    entry_point='gym_foo.envs:FooEnv',
+)
+register(
+    id='oddoreven-extrahard-v0',
+    entry_point='gym_oddoreven.envs:FooExtraHardEnv',
+)
