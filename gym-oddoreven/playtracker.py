30a093d5132db967e854ffbbeec155a7ce0d6dc7
abhijithneilabraham <35420019+abhijithneilabraham@users.noreply.github.com>
2018-09-11 23:31:43 +0530

    Create playtracker.py

---
+++ gym-oddoreven/playtracker.py
@@ -0,0 +1,6 @@
+import random
+generate=[random.randint(1,5) for x in range(0,100)] #computer generating random numbers for learning and total 100 numbers in range 1-5 is generated
+input=[input("enter a number") for y in range(0,100)] #enter the user input
+
+
+
