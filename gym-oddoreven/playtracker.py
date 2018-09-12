641ab4075f0acc0f15937ca669bc7bc834f71995
abhijithneilabraham <35420019+abhijithneilabraham@users.noreply.github.com>
2018-09-12 19:19:13 +0530

    Update playtracker.py

--- gym-oddoreven/playtracker.py
+++ gym-oddoreven/playtracker.py
@@ -1,6 +1,9 @@
 import random
-generate=[random.randint(1,5) for x in range(0,100)] #computer generating random numbers for learning and total 100 numbers in range 1-5 is generated
-input=[input("enter a number") for y in range(0,100)] #enter the user input
-
-
-
+n=100
+generate=[random.randint(1,5) for x in range(0,n)] #computer generating random numbers for learning and total 100 numbers in range 1-5 is generated
+user_input=[int(input("enter a number")) for y in range(0,n)] #enter the user input
+c = [0]*n
+for i in range(0,n):
+    if(generate[i] == user_input[i]):
+        c[i]=1
+print(user_input,generate,c)        
