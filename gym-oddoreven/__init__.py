4cfd5bbb5d9869519aff92f129f5cc656a325da7
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-13 22:24:22 +0530

    Update __init__.py

--- gym-oddoreven/__init__.py
+++ gym-oddoreven/__init__.py
@@ -5,7 +5,3 @@ register(
     id='oddoreven-v0',
     entry_point='gym_oddoreven.envs:oddoreven_env',
 )
-register(
-    id='oddoreven-extrahard-v0',
-    entry_point='gym_oddoreven.envs:FooExtraHardEnv',
-)
